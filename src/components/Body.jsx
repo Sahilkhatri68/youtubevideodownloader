703fc7ac30c8d4fabfcb8ff97d84c2f645f1b182
sahilkhatri68 <sahilkhatriss01@gmail.com>
2022-09-17 16:05:49 +0530

    final

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,7 +3,11 @@ import React, { useEffect } from 'react'
 import { useState } from 'react'
 import "./body.css"
 import EastIcon from '@mui/icons-material/East';
-
+import DownloadIcon from '@mui/icons-material/Download';
+import MicOffIcon from '@mui/icons-material/MicOff';
+import VolumeDownIcon from '@mui/icons-material/VolumeDown';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
+// import LinearProgress from '@mui/material/LinearProgress';
 export default function Body() {
     const [data, setData] = useState(null)
     const [isLoading, setIsLoading] = useState(false);
@@ -27,74 +31,68 @@ export default function Body() {
     }, [])
     return (
         <>
-
-            {/* <div style={{
-
-                textAlign: "center",
-                justifyContent: "center"
-            }}>
-                <div>
-                    <div  >
-                        <h1>Youtube video downloader</h1>
-
-                    </div>
-                </div>
-                <div>
-                    <div>
-                        <input placeholder='Paste your link here' style={{ padding: 10, fontSize: 18 }} name={url}
-                            onChange={(e) => setUrl(e.target.value)}
-                        />
-
-                        <select name="cars" id="cars" style={{ padding: 10, fontSize: 18 }} >
-
-
-                            <option value="saab">1080p</option>
-                            <option value="opel">720p</option>
-                            <option value="audi">420p</option>
-                            <option value="audi">360p</option>
-                            <option value="audi">240p</option>
-                            <option value="audi">144p</option>
-                        </select>
-                    </div>
-                </div>
-                <div style={{ marginTop: 20 }}>
-                    <button onClick={() => getvideo()} > {isLoading == true ? "Get Video" : ("Video found....")}</button>
-                </div>
-
-                <div style={{ marginTop: 20 }}>
-                    {data ? (
-                        <iframe style={{ width: 300, height: 300 }} src={data.url}>
-
-                        </iframe>
-                    ) : (0)}
-                </div>
-
-                <div>Available Formats </div>
-                <div>
-
-                </div>
-            </div> */}
-
             <div className='maincont'>
                 <div className='insidemain'>
                     <div className='headdiv'>
-                        <h2 style={{ color: "rgb(116 116 116)" }}>Youtube Video downloader</h2>
+                        {/* <h2 style={{ color: "rgb(116 116 116)" }}>Youtube Video downloader</h2> */}
                     </div>
                     <div className="bottombox">
                         <div className="inputbtn">
                             <div className="inputdiv">
-                                <input className='leftip' placeholder='Paste your link here...'></input>
+                                <input className='leftip' placeholder='Paste your link here...' name={url}
+                                    onChange={(e) => setUrl(e.target.value)}></input>
                             </div>
                             <div className="btndivip">
-                                <button className='btnip'>Start <EastIcon style={{ marginLeft: 5 }} /> </button>
+                                <button className='btnip' onClick={() => getvideo()}>Start <EastIcon style={{ marginLeft: 5 }} /> </button>
                             </div>
                         </div>
                     </div>
                     <div className='terms'>
                         <p>By using our service you are accepting our terms of service.</p>
                     </div>
+
+                    {data ? (<>
+                        <div className='framediv'>
+                            <div className="leftframe">
+                                <img src={data.thumb.high.url} style={{ width: "100%" }}>
+                                </img>
+                            </div>
+
+                            <div className="rightframe">
+                                <table className='righttable'>
+                                    <tr className='stickpostion'>
+                                        <th> Resolution</th>
+                                        <th> Type</th>
+                                        <th> Sound</th>
+                                        <th> Download</th>
+                                    </tr>
+                                    {data.info.map((value) => {
+                                        return (
+                                            <>
+                                                <tr>
+                                                    <td> {value.qualityLabel !== null ?  (`${value.qualityLabel}`):("Audio") }   </td>
+                                                    <td>{value.container}</td>
+                                                    <td>{value.hasAudio === false ? (<MicOffIcon />) : (<VolumeUpIcon />)} </td>
+                                                    <td >
+                                                        <a href={value.url}>  <button className='tabledata'>
+                                                            <div style={{ marginTop: 3, fontWeight: 500 }}> </div>
+                                                            Download
+                                                            <div style={{ marginTop: 3, marginLeft: 3 }}><DownloadIcon /></div>
+                                                        </button></a>
+                                                    </td>
+                                                </tr>
+                                            </>
+                                        )
+                                    })}
+
+
+                                </table>
+                            </div>
+
+                        </div>
+                    </>) : ""}
                 </div>
-          
+
             </div>
 
         </>
